903f4688f248ee24432e4b93f696bf95b1c99e3d
Enzo Marco <116755774+EnzoMarco@users.noreply.github.com>
2024-04-03 19:30:32 -0300

    Add files via upload

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,23 @@
+/* 
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+function verificarChute() {
+    console.log('O botão foi clicado');
+}
+function mensagem() {
+    alert("EU AMO JS!");
+}
+function viajou() {
+    let cidade = prompt("Digite uma cidade do Brasil:");
+    alert(`Estive em ${cidade} e lembrei de você`); 
+}
+function soma() {
+    let numero1 = parseInt(prompt('digite um numero:'));
+    let numero2 = parseInt(prompt('digite outro numero:'));
+    let resultado = (numero1 + numero2) ;
+    alert(`o resultado da soma dos números é ${resultado}`);
+}
+
+*/
+
