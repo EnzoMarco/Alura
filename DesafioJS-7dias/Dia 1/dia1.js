8152e7f525afe70de7362ac4b4437997ce4ad33c
Enzo Marco <enzomarco100@gmail.com>
2024-04-10 22:55:05 -0300

    Lançar

---
+++ DesafioJS-7dias/Dia 1/dia1.js
@@ -0,0 +1,27 @@
+let numeroUm = 1;
+let numeroUmString = '1';
+let numeroTrinta = 30;
+let numeroTrintaString = '30';
+let numeroDez = 10;
+let numeroDezString = '10';
+
+if(numeroUm == numeroUmString) {
+    console.log('As variáveis tem o mesmo valor, mas tipos diferentes');
+}
+    else{
+        console.log('As variáveis não tem o mesmo valor.');
+    }
+
+if(numeroTrinta === numeroTrintaString) {
+    console.log('As variáveis tem o mesmo valor, mas tipos diferentes');
+}
+    else{
+        console.log('As variáveis não tem o mesmo tipo.');
+    }
+
+if(numeroDez == numeroDezString) {
+    console.log('As variáveis tem o mesmo valor, mas tipos diferentes');
+}
+    else{
+        console.log('As variáveis não tem o mesmo valor.');
+    }
