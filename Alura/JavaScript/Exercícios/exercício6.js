5dbc66b1cc6c8849ff91dc6f75fc52f9f461604d
Enzo Marco <enzomarco100@gmail.com>
2024-04-15 21:08:29 -0300

    exercício6

---
+++ Alura/JavaScript/Exercícios/exercício6.js
@@ -0,0 +1,12 @@
+function paridade(a){
+    if(a == 0){
+        alert('Par');
+    }   
+    else if(a % 2 == 0) {
+        alert('Par');
+    }
+    else{
+        alert('Ímpar');
+    }
+}
+paridade(127);
